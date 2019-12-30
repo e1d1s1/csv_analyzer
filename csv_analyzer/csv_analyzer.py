2aa6f58846cb538fff8bf772756b3bb93492db56
eric <eric@BEDROOMWINDOWS.home>
2019-12-29 23:17:52 -0500

    PEP linting cleanup

--- csv_analyzer/csv_analyzer.py
+++ csv_analyzer/csv_analyzer.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
+######################################################################
+#  CSV Plotting Utility
+#
+#  ------------------------------------------------------------------
+#  Author : Eric D. Schmidt
+#  Language: Python 3
+#  License: MIT
+#  ------------------------------------------------------------------
+#
+#  Copyright (c) 2019 Eric D. Schmidt
+######################################################################
+
+
 import matplotlib.pyplot as plotter
 import matplotlib.colors as mcolors
 from matplotlib.lines import Line2D
@@ -33,9 +46,11 @@ parser.add_argument('-t', '--filter', metavar='expression', type=str,
 parser.add_argument('-i', '--title', type=str,
                     help='title of plot', default='')
 parser.add_argument('-s', '--sessionstart', action='store_true',
-                    help='starts a new session so we only load data/assign colors once', default='')
+                    help='starts a new session so we only load data & assign colors once', 
+                    default='')
 parser.add_argument('-c', '--sessioncontinue', action='store_true',
-                    help='Continues an existing session so we only load data/assign colors once', default='')
+                    help='Continues an existing session so we only load data & assign colors once',
+                    default='')
 parser.add_argument('-m', '--terminate', action='store_true',
                     help='Closes immediately after data load and session save', default='')
 parser.add_argument('--scatter', action='store_true',
@@ -48,48 +63,50 @@ args = parser.parse_args()
 if len(args.file) == 0 or len(args.columns_plot) == 0:
     parser.print_help()
     exit(1)
-    
+
 if not args.xaxis is None and len(args.xaxis) != 0:
     xaxis_label = args.xaxis
 else:
-    i = 0
+    col_cnt = 0
     for key in args.columns_plot:
-        if i % 2 == 0:
+        if col_cnt % 2 == 0:
             if len(xaxis_label) > 0: 
                 xaxis_label += ","
             xaxis_label += key
-        i += 1
+        col_cnt += 1
 
-def add_line(axis, key, lines_dict, color_dict):
+def add_line(axis, colname, lines_dict, color_dict):
+    '''adds a line to the plot. Assigns color and style'''
     global line_cnt
 
-    c = 'black'
-    th = 0.4
+    color_name = 'black'
+    thickness = 0.4
     if args.colorbyplot:
-        c = color_palette[line_cnt][0]
-        th = color_palette[line_cnt][1]
+        color_name = color_palette[line_cnt][0]
+        thickness = color_palette[line_cnt][1]
     else:
-        c = color_dict[key][0]
-        th = color_dict[key][1]
-    lines_dict[key] = Line2D([], [], color=c, linewidth=th)
-    axis.add_line(lines_dict[key])
+        color_name = color_dict[colname][0]
+        thickness = color_dict[colname][1]
+    lines_dict[colname] = Line2D([], [], color=color_name, linewidth=thickness)
+    axis.add_line(lines_dict[colname])
     line_cnt+=1
 
 def create_lines(axis, lines_dict, color_dict):
+    '''depending on plot type adds a line and legend entry to plot'''
     global line_cnt
     global legend_keys
 
     line_cnt = 0
     i = 0
-    for key in args.columns_plot:
+    for col in args.columns_plot:
         if not args.scatter or i % 2 != 0:
-            add_line(axis, key, lines_dict, color_dict)
-            legend_keys.append(key)
+            add_line(axis, col, lines_dict, color_dict)
+            legend_keys.append(col)
         i += 1
 
 
 def assign_colors(color_dict):
-    # automatically pick good looking colors
+    '''automatically pick good looking colors'''
     colors = dict(mcolors.BASE_COLORS, **mcolors.CSS4_COLORS)
     by_hsv = sorted((tuple(mcolors.rgb_to_hsv(mcolors.to_rgba(color)[:3])), name)
                     for name, color in colors.items())
@@ -101,14 +118,14 @@ def assign_colors(color_dict):
     if len(color_dict) > 0:
         thickness_increment = (max_thickness - start_thickness) / len(color_dict.keys())
     cnt = 0
-    for key in color_dict:
+    for color in color_dict:
         if cnt <= 2 and args.colorbyplot:
             if cnt == 0:
-                color_dict[key] = ("blue", start_thickness)
+                color_dict[color] = ("blue", start_thickness)
             elif cnt == 1:
-                color_dict[key] = ("red", start_thickness)
+                color_dict[color] = ("red", start_thickness)
             elif cnt == 2:
-                color_dict[key] = ("green", start_thickness)
+                color_dict[color] = ("green", start_thickness)
         else:
             luminosity = 1
             index = 0
@@ -119,31 +136,32 @@ def assign_colors(color_dict):
                 rgb = mcolors.to_rgba(color_name)
                 luminosity = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2]
 
-            color_dict[key] = (color_name, thickness)
+            color_dict[color] = (color_name, thickness)
             if cnt % 2 == 0:
                 thickness += thickness_increment
-        
-        color_palette.append(color_dict[key])
-        cnt+=1
+
+        color_palette.append(color_dict[color])
+        cnt += 1
 
 def fill_lines(lines_dict, ydata_dict):
+    '''fill lines with associated data'''
     if args.scatter:
         i = 0
-        for key in args.columns_plot:
+        for col in args.columns_plot:
             if i % 2 == 0:
-                xdata = ydata_dict[key]
-                lines_dict.pop(key)
+                xdata = ydata_dict[col]
+                lines_dict.pop(col)
             else:
-                d = ydata_dict[key]
-                lines_dict[key].set_data(xdata, d)
-            i+=1
+                ydata = ydata_dict[col]
+                lines_dict[col].set_data(xdata, ydata)
+            i += 1
     else:
-        for key in lines_dict:
-            d = ydata_dict[key]
+        for line_name in lines_dict:
+            ydata = ydata_dict[line_name]
             xdata = x_axis
 
-            if not d is None:
-                lines_dict[key].set_data(xdata, d)
+            if not ydata is None:
+                lines_dict[line_name].set_data(xdata, ydata)
             else:
                 return False
 
@@ -151,26 +169,27 @@ def fill_lines(lines_dict, ydata_dict):
 
 
 def fit_plot(axis, lines, ydata_dict):
+    '''figure out good bounding view for plot'''
     min_y = float('inf')
     max_y = float('-inf')
-    min_x = float('inf')
-    max_x = float('-inf')
+    min_x_axis = float('inf')
+    max_x_axis = float('-inf')
 
     if args.scatter:
         i = 0
-        for key in args.columns_plot:
+        for col in args.columns_plot:
             if i % 2 == 0:
-                min_x = min(min_x, min(ydata_dict[key]))
-                max_x = max(max_x, max(ydata_dict[key]))
+                min_x_axis = min(min_x_axis, min(ydata_dict[col]))
+                max_x_axis = max(max_x_axis, max(ydata_dict[col]))
             i+=1
     else:
-        min_x = x_axis[0]
-        max_x = x_axis[-1]
+        min_x_axis = x_axis[0]
+        max_x_axis = x_axis[-1]
 
-    for key in lines:
-        if key in ydata_dict and len(ydata_dict[key]) > 0:
-            min_y = min(min_y, min(ydata_dict[key]))
-            max_y = max(max_y, max(ydata_dict[key]))
+    for line_name in lines:
+        if line_name in ydata_dict and len(ydata_dict[line_name]) > 0:
+            min_y = min(min_y, min(ydata_dict[line_name]))
+            max_y = max(max_y, max(ydata_dict[line_name]))
 
     if max_y != 0:
         max_y += 5
@@ -179,18 +198,19 @@ def fit_plot(axis, lines, ydata_dict):
         min_y -= 5
 
     axis.set_ylim(min_y, max_y)
-    axis.set_xlim(min_x, max_x)
+    axis.set_xlim(min_x_axis, max_x_axis)
     axis.set_xlabel(xaxis_label)
     axis.grid(True)
 
 
 def get_csv_data(filename, columns, xaxis_name, rowstart, rowend):
+    '''parse the raw CSV data from the source file'''
     headers = {}
     data = {}
     xaxis = []
 
-    for key in columns:
-        data[key] = []
+    for col in columns:
+        data[col] = []
 
     row_count = 0
     row_num = 0
@@ -220,13 +240,13 @@ def get_csv_data(filename, columns, xaxis_name, rowstart, rowend):
             elif row_num >= rowstart:
                 i = 0
                 for ele in row:
-                    for idx, name in headers.items():
-                        if idx == i:
+                    for col_idx, name in headers.items():
+                        if col_idx == i:
                             data[name].append(float(ele))                    
                     if i == idx_xaxis:
                         xaxis.append(float(ele))
                     i += 1
-                    
+
             if idx_xaxis < 0 and row_num > 0:
                 xaxis.append(float(row_num - 1))
 
@@ -254,15 +274,16 @@ if args.sessioncontinue:
 
     print ('restoring session data')
     try:
-      while os.path.exists('csvsession.pickle.lock'):
-        print("waiting for lock on pickle file")
-        time.sleep(2)
+      # check if file is still being used
+        while os.path.exists('csvsession.pickle.lock'):
+            print("waiting for lock on pickle file")
+            time.sleep(2)
 
-      with open("csvsession.pickle", "rb") as f:
-          dict_data = pickle.load(f)
-          x_axis = pickle.load(f)
-          dict_colors = pickle.load(f)
-          color_palette = pickle.load(f)
+        with open("csvsession.pickle", "rb") as f:
+            dict_data = pickle.load(f)
+            x_axis = pickle.load(f)
+            dict_colors = pickle.load(f)
+            color_palette = pickle.load(f)
 
     except:  # handle other exceptions
         print("Unexpected error:", sys.exc_info()[0])
@@ -273,10 +294,11 @@ else:
 
     try:
         print ('reading CSV data')
-        dict_data, x_axis = get_csv_data(args.file, args.columns_plot, xaxis_label, args.rowstart, args.rowend)
+        dict_data, x_axis = get_csv_data(args.file, args.columns_plot,
+                                         xaxis_label, args.rowstart, args.rowend)
 
-    except IOError as e:
-        print ("I/O error({0}): {1}".format(e.errno, e.strerror))
+    except IOError as err:
+        print("I/O error({0}): {1}".format(err.errno, err.strerror))
         exit(1)
     except:  # handle other exceptions
         print ("Unexpected error:", sys.exc_info()[0])
@@ -319,30 +341,30 @@ if len(args.filter) > 0:
             found_idx = 0    
             for token in tokens:                
                 if token == key_name:
-                     replacements[found_idx] = "np.array(dict_data[\"" + key_name + "\"])"
-                found_idx+=1
-               
+                    replacements[found_idx] = "np.array(dict_data[\"" + key_name + "\"])"
+                found_idx += 1
+
         filter_expression = ""
         for replacement in replacements:
             tokens[replacement] = replacements[replacement]        
-        
+
         for token in tokens:
             filter_expression += token + " "
 
         tokens = filter_expression.split("AND")
         if len(tokens) > 1:
             filter_expression = ""
-            for i in range(len(tokens)):
-                filter_expression += "(" + tokens[i] + ") "
-                if i < len(tokens) - 1:
+            for token_idx in range(len(tokens)):
+                filter_expression += "(" + tokens[token_idx] + ") "
+                if token_idx < len(tokens) - 1:
                     filter_expression += " & "
 
         tokens = filter_expression.split("OR")
         if len(tokens) > 1:
             filter_expression = ""
-            for i in range(len(tokens)):
-                filter_expression += "(" + tokens[i]+ ") "
-                if i < len(tokens) - 1:
+            for token_idx in range(len(tokens)):
+                filter_expression += "(" + tokens[token_idx]+ ") "
+                if token_idx < len(tokens) - 1:
                     filter_expression += " | "
 
         exe_str = "res = np.where(" + filter_expression + ")"
@@ -352,6 +374,7 @@ if len(args.filter) > 0:
     res = []
     exec(exe_str)
 
+    # hightlight the selected data range(s)
     if len(res) > 0:
         arr = res[0]
         last_idx = -2
@@ -370,8 +393,9 @@ if len(args.filter) > 0:
         if not highlighted and min_x != max_x:
             plotter.axvspan(min_x, max_x, color='orange', alpha=0.5)
 
-
+# reload CSV data from the pickle file
 if args.sessionstart:
+    # indicate that file is in use
     lockfile = open('csvsession.pickle.lock', 'w+')
     lockfile.close()
     with open("csvsession.pickle", "wb") as f:
@@ -380,7 +404,7 @@ if args.sessionstart:
         pickle.dump(dict_colors, f)
         pickle.dump(color_palette, f)
 
-    
+
     if os.path.exists('csvsession.pickle.lock'):
         os.remove('csvsession.pickle.lock')
 
@@ -396,5 +420,3 @@ if doshow:
     plotter.show(block=True)
 
 exit(0)
-
-
